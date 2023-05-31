2f3842eccc19516328a1a05b1bb30f798bacfcc7
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-05-31 10:50:38 +0000

    Fix publisher type

--- waypoint/waypoint_manager/src/waypoint_manager.cpp
+++ waypoint/waypoint_manager/src/waypoint_manager.cpp
@@ -8,7 +8,7 @@ WaypointManager::WaypointManager(ros::NodeHandle &nh, ros::NodeHandle &pn)
     pn.param<std::string>("topic_waypoints", topic_waypoints, "wpLoader/waypoints");
     pn.param<std::string>("service_manager_set", service_manager_set, "wpManager/set");
 
-    _nowWp_publisher = nh.advertise<waypoint_msgs::waypoints>(topic_nowWp, 1);
+    _nowWp_publisher = nh.advertise<waypoint_msgs::waypoint>(topic_nowWp, 1);
     _wps_subscriber = nh.subscribe(topic_waypoints, 1, &WaypointManager::waypoints_cb, this);
     _manager_set_server = nh.advertiseService(service_manager_set, &WaypointManager::manager_set_cb, this);
 
