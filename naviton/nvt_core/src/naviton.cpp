877e688180ef660889d37ef7068d7570f465b6b9
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-11-01 07:35:30 +0000

    Fix typo

--- naviton/nvt_core/src/naviton.cpp
+++ naviton/nvt_core/src/naviton.cpp
@@ -5,7 +5,7 @@ Naviton::Naviton(ros::NodeHandle &nh, ros::NodeHandle &pn)
     std::string service_wpManager_set, topic_nowWp_local;
     
     pn.param<std::string>("service_wpManager_set", service_wpManager_set, "/naviton/waypoint/wpManager/set");
-    pn.param<std::string>("topic_nowWp_local", topic_nowWp_local, "/naviton//waypoint/wpManager/nowWp_local");
+    pn.param<std::string>("topic_nowWp_local", topic_nowWp_local, "/naviton/waypoint/wpManager/nowWp_local");
 
     _wpManager_set_client = nh.serviceClient<waypoint_manager_msgs::waypoint_manager_set>(service_wpManager_set);
     _nowWp_local_subscriber = nh.subscribe(topic_nowWp_local, 10, &Naviton::nowWp_local_cb, this);
