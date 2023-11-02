eb811e673f119ba42e1dfbcf8b276b1271cc3382
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-11-02 05:21:15 +0000

    Update nvt_core for nvt_plugin

--- naviton/nvt_core/include/nvt_core/naviton.h
+++ naviton/nvt_core/include/nvt_core/naviton.h
@@ -3,6 +3,8 @@
 
 #include <ros/ros.h>
 
+#include <std_srvs/Empty.h>
+
 #include <waypoint_msgs/waypoint.h>
 #include <waypoint_manager_msgs/waypoint_manager_set.h>
 
@@ -13,10 +15,18 @@ class Naviton
         void init();
         void update();
         void nowWp_local_cb(waypoint_msgs::waypointConstPtr msg);
+
+        bool start_cb(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res);
+        bool pause_cb(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res);
+
     private:
+        ros::ServiceServer _nvt_start_server;
+        ros::ServiceServer _nvt_pause_server;
         ros::ServiceClient _wpManager_set_client;
         ros::Subscriber _nowWp_local_subscriber;
         waypoint_msgs::waypoint _nowWp_local;
+
+        bool _paused;
 };
 
 #endif
