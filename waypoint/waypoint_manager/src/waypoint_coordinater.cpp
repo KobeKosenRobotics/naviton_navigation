3d9b7aa492f7ba547b6c9f9d1ef26e083f4aa6b6
miyamiyamon <r118168@g.kobe-kosen.ac.jp>
2025-03-08 03:58:40 +0000

    fix bug

--- waypoint/waypoint_manager/src/waypoint_coordinater.cpp
+++ waypoint/waypoint_manager/src/waypoint_coordinater.cpp
@@ -1,10 +1,74 @@
+#include "waypoint_manager/waypoint_coordinater.h"
+
+WaypointCoordinater::WaypointCoordinater(ros::NodeHandle &nh, ros::NodeHandle &pn)
+{
+    // トピック名やサービス名をパラメータから取得
+    std::string topic_absolute_position;
+    pn.param<std::string>("topic_absolute_position", topic_absolute_position, "wpManager/absolute_position");
+    abs_wp_publisher = nh.advertise<geometry_msgs::PoseStamped>(topic_absolute_position, 10);
+
+    std::string topic_waypoints;
+    pn.param<std::string>("topic_waypoints", topic_waypoints, "wpLoader/waypoints");
+    waypoints_sub = nh.subscribe(topic_waypoints, 10, &WaypointCoordinater::waypointsCallback, this);
+    
+    // YAMLファイルから任意の絶対座標とオリエンテーションを設定
+    double absolute_x, absolute_y, absolute_z;
+    double absolute_qx, absolute_qy, absolute_qz, absolute_qw;
+    
+    pn.param("absolute_x", absolute_x, 0.0);
+    pn.param("absolute_y", absolute_y, 0.0);
+    pn.param("absolute_z", absolute_z, 0.0);
+    pn.param("absolute_orientation_x", absolute_qx, 0.0);
+    pn.param("absolute_orientation_y", absolute_qy, 0.0);
+    pn.param("absolute_orientation_z", absolute_qz, 0.0);
+    pn.param("absolute_orientation_w", absolute_qw, 1.0);
+
+    // _absolute_positionをtf2::Transformで宣言し、座標と回転を設定
+    tf2::Quaternion abs_quat(absolute_qx, absolute_qy, absolute_qz, absolute_qw);
+    _absolute_position.setOrigin(tf2::Vector3(absolute_x, absolute_y, absolute_z));
+    _absolute_position.setRotation(abs_quat);
+
+    // TFバッファとリスナーの初期化
+    tf_buffer = std::make_shared<tf2_ros::Buffer>();
+    tf_listener = std::make_shared<tf2_ros::TransformListener>(*tf_buffer);
+}
+
+geometry_msgs::Pose WaypointCoordinater::calculateRelativePosition(const geometry_msgs::Pose &target, const geometry_msgs::Pose &current)
+{
+    geometry_msgs::Pose relative_pose;
+
+    // 目標位置と現在位置の相対座標計算
+    relative_pose.position.x = target.position.x - current.position.x;
+    relative_pose.position.y = target.position.y - current.position.y;
+    relative_pose.position.z = target.position.z - current.position.z;
+
+    // 回転の計算（クォータニオンの相対回転）
+    tf2::Quaternion q_target, q_current, q_relative;
+    tf2::fromMsg(target.orientation, q_target);  // geometry_msgs::Quaternion -> tf2::Quaternion
+    tf2::fromMsg(current.orientation, q_current);
+
+    // 相対回転 = 現在の回転の逆 × 目標の回転
+    q_relative = q_current.inverse() * q_target;
+
+    // 計算したクォータニオンを代入
+    relative_pose.orientation = tf2::toMsg(q_relative);  // tf2::Quaternion -> geometry_msgs::Quaternion
+
+    return relative_pose;
+}
+
+void WaypointCoordinater::waypointsCallback(const waypoint_msgs::waypoints::ConstPtr &msg)
+{
+    _wps = *msg; // 受け取ったデータを保存
+    ROS_INFO("Waypoints received: %zu waypoints", _wps.waypoints.size());
+}
+
 void WaypointCoordinater::publish()
 {
-    // 現在の位置を取得（base_link -> map のTFを取得）
+    // 現在の位置（tf2を使ってbase_link座標を取得）
     geometry_msgs::Pose current_wp_pose;
     try
     {
-        geometry_msgs::TransformStamped transform = tf_buffer->lookupTransform("map", "naviton/base_link", ros::Time(0), ros::Duration(1.0));
+        geometry_msgs::TransformStamped transform = tf_buffer->lookupTransform("naviton/base_link", "map", ros::Time(0), ros::Duration(1.0));
         current_wp_pose.position.x = transform.transform.translation.x;
         current_wp_pose.position.y = transform.transform.translation.y;
         current_wp_pose.position.z = transform.transform.translation.z;
@@ -16,41 +80,32 @@ void WaypointCoordinater::publish()
         return;
     }
 
-    // 任意のウェイポイントを取得
-    int wp_number = 1;  // 例えば、3番目のウェイポイント
-    if (wp_number >= _wps.waypoints.size())
-    {
-        ROS_WARN("Waypoint index out of range");
-        return;
-    }
+    // 任意のウェイポイント番号を指定し、その座標を基に相対座標を計算
+    int wp_number = 1;  // 任意のウェイポイント番号（例えば、3番目のウェイポイント）
     waypoint_msgs::waypoint waypoint_to_calculate = _wps.waypoints[wp_number];
 
-    // 相対座標を計算
+    // 相対位置を計算
     geometry_msgs::Pose relative_pose = calculateRelativePosition(waypoint_to_calculate.pose.pose, current_wp_pose);
 
-    // 絶対座標を計算（相対座標をmap座標系に変換）
+    // 絶対座標を計算（YAML設定された絶対座標を基に相対座標を足す）
     geometry_msgs::PoseStamped absolute_pose;
-    absolute_pose.header.stamp = ros::Time::now();
-    absolute_pose.header.frame_id = "map";  // map座標系でパブリッシュ
 
-    // tf2を使って座標変換
-    tf2::Transform tf_absolute;
-    tf_absolute.setOrigin(_absolute_position.getOrigin());  // YAMLで設定した絶対座標
-    tf_absolute.setRotation(_absolute_position.getRotation());
+    // tf2ベクトルに変換して回転を適用
+    tf2::Vector3 relative_position(relative_pose.position.x*10000, relative_pose.position.y*10000, relative_pose.position.z*10000);
+    tf2::Vector3 absolute_position = _absolute_position.getOrigin() + relative_position;
 
-    tf2::Vector3 relative_position(relative_pose.position.x/100000, relative_pose.position.y/100000, relative_pose.position.z);
-    tf2::Vector3 absolute_position = tf_absolute * relative_position;
-
-    absolute_pose.pose.position.x = absolute_position.x();
-    absolute_pose.pose.position.y = absolute_position.y();
-    absolute_pose.pose.position.z = absolute_position.z();
+    absolute_pose.pose.position.x = _absolute_position.x();
+    absolute_pose.pose.position.y = _absolute_position.y();
+    absolute_pose.pose.position.z = _absolute_position.z();
 
     // 絶対オリエンテーションの計算
-    tf2::Quaternion relative_quat, abs_quat;
-    tf2::fromMsg(relative_pose.orientation, relative_quat);
-    abs_quat = _absolute_position.getRotation();
+    tf2::Quaternion abs_quat = _absolute_position.getRotation();
 
+    // 相対回転を取得し、最終的なオリエンテーションを計算
+    tf2::Quaternion relative_quat;
+    tf2::fromMsg(relative_pose.orientation, relative_quat);
     tf2::Quaternion final_orientation = abs_quat * relative_quat;
+
     absolute_pose.pose.orientation = tf2::toMsg(final_orientation);
 
     // 絶対座標をパブリッシュ
